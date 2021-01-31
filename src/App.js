5831cd1c369a0773d41b92d8c0a5aa3387e0308e
azuravlovs <zuravlovsandris@gmail.com>
2021-01-31 09:20:51 +0200

    broken homepage in smaller components, finished styling of MenuItems comp.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-  import React from 'react';
-import HomePage from './HomePage';
+import React from 'react';
+import HomePage from './pages/homepage/HomePage';
 
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div className='App'>
+      <div>
         <HomePage />
       </div>
     );
---
+++ src/pages/homepage/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './homepage.style.scss'
+import Directory from '../../componenets/directory/Directory'
+
+export default function HomePage() {
+    return (
+        <div className='homepage'>
+          <Directory />
+      </div>
+    )
+}
